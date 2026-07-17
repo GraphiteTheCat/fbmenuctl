d4d0a41cc8cd564d3742d4c5491b54755635a8ff
Graphite the Cat <58424443+GraphiteTheCat@users.noreply.github.com>
2026-07-17 03:31:03 +0200

    Fixed the installer

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ chmod +x ./fbmenuctl_gen.sh
 chmod +x ./fbmenuctl_gui.py
 
 #Move files
-mv ./fbmenuctl /bin/fbmenuctl
-mv ./fbmenuctl_gen.sh /bin/fbmenuctl_gen.sh
-mv ./fbmenuctl_gui.py /usr/libexec/fbmenuctl_gui.py
-mkdir -p "$HOME/.local/share/fbmenuctl"
+sudo mv ./fbmenuctl /bin/fbmenuctl
+sudo mv ./fbmenuctl_gen.sh /bin/fbmenuctl_gen.sh
+sudo mv ./fbmenuctl_gui.py /usr/libexec/fbmenuctl_gui.py
+sudo mkdir -p "$HOME/.local/share/fbmenuctl"
 for FILE in "$RELDIR/Categories/"*; do
-	mv "$FILE" "$HOME/.local/share/fbmenuctl/$FILE"
+	mv "$FILE" "$HOME/.local/share/fbmenuctl/$(basename $FILE)"
 done
